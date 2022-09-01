e6e80f1b0badd93d6e8b039ff4b9eb3d576aaf1f
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-09-01 00:20:20 -0500

    Problemas con el Interfaz Home

--- app/src/main/java/com/jesus/testdefinitivo2022/actividades30/SplashActivity.java
+++ app/src/main/java/com/jesus/testdefinitivo2022/actividades30/SplashActivity.java
@@ -2,8 +2,11 @@ package com.jesus.testdefinitivo2022.actividades30;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.jesus.testdefinitivo2022.MainActivity;
 import com.jesus.testdefinitivo2022.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
 }
---
+++ app/src/main/java/com/jesus/testdefinitivo2022/pantallahome_orig.java
@@ -0,0 +1,14 @@
+package com.jesus.testdefinitivo2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallahome_orig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallahome_orig);
+    }
+}
