f0a66fc83a3dcda9b16ed73e513a35526ab8129f
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-09-10 19:21:00 -0500

    Bugs/10/09/2022

---
+++ app/src/main/java/com/jesus/testdefinitivo2022/actividades30/creacioncuenta.java
@@ -0,0 +1,14 @@
+package com.jesus.testdefinitivo2022.actividades30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class creacioncuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacioncuenta);
+    }
+}
